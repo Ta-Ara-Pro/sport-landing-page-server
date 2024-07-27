965cd929fa923f25cff4f9c6085804c698312427
heren.pro <167914557+Ta-Ara-Pro@users.noreply.github.com>
2024-07-28 02:04:43 +0330

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Configure CORS
 app.use(cors({
     origin: 'https://sport-landing-page.vercel.app',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
+
 }));
 
 app.use(express.json());
