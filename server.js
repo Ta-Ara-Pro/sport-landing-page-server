f1b3c5636876c6ea876df6b662a5d5ebb26b3d31
Ta-Ara-Pro <tahereh.q.ara@gmail.com>
2024-07-28 22:27:56 +0330

    edit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Configure CORS
 app.use(cors({
-    origin: 'https://sport-landing-page.vercel.app',
+    origin: 'https://sport-landing-page-server.vercel.app',
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
